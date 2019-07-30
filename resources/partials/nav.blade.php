9eb797aa0756c2da33fe0394615460b627a1e357
Joaquin Pibernat <joaquin.pibernat@gmail.com>
2019-07-30 08:22:36 -0300

    Projecto Ferswim en Laravel Digitahouse

---
+++ resources/partials/nav.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <ul class="navlink">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Preguntas</a></li>
+            <li><a href="index.php"><img src="img/logo.png" alt=""></a></li>
+            <li><a href="#">Registrate</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+        
+    </nav>
+</body>
+</html>
