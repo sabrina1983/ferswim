a335a387227ce32cfb336595aa7ba31d2247dfc6
Joaquin Pibernat <joaquin.pibernat@gmail.com>
2019-08-12 02:14:18 -0300

    se sube modificacion de ruta y controlador para el carrito.

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function list() {
+        dd("Esto es un echo de la funcion listar en el controlador del Carrito, esta funcion deberia retornar la lista de los productos agregados al carrito por el usuario, dando la posibilidad de editar el contenido del mismo.");
+    }
 }
